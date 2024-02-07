71d5fec005faa92507d0030060a15c056344691f
PeterBro <p3t3rbr0@gmail.com>
2024-02-07 19:53:15 +0300

    Get rid of side effect when getting profiler result

--- pure_utils/debug.py
+++ pure_utils/debug.py
@@ -17,6 +17,108 @@ DEFAULT_STACK_SIZE: int = 10
 DEFAULT_STACK_FRAME: int = 2
 
 
+class ProfileResultPresenter:
+    def __init__(self, stats: Stats, amount: int) -> None:
+        self.stats = stats
+        self.amount = amount
+
+    @property
+    def indent(self) -> str:
+        return " " * 8
+
+    @property
+    def title(self) -> str:
+        return "   ncalls  tottime  percall  cumtime  percall filename:lineno(function)"
+
+    def f8(self, x: float) -> str:
+        return f"{x:8.3f}"
+
+    def func_std_string(self, func_name) -> str:
+        if func_name[:2] == ("~", 0):
+            # special case for built-in functions
+            name = func_name[2]
+            if name.startswith("<") and name.endswith(">"):
+                return "{%s}" % name[1:-1]
+            else:
+                return name
+        else:
+            return "%s:%d(%s)" % func_name
+
+    def print_line(self, func) -> str:
+        lines = []
+        cc, nc, tt, ct, callers = self.stats.stats[func]
+        c = str(nc)
+
+        if nc != cc:
+            c = c + "/" + str(cc)
+
+        lines.append(f"{c.rjust(9)} ")
+        lines.append(f"{self.f8(tt)} ")
+
+        if nc == 0:
+            lines.append(f"{self.indent} ")
+        else:
+            lines.append(f"{self.f8(tt/nc)} ")
+
+        lines.append(f"{self.f8(ct)} ")
+
+        if cc == 0:
+            lines.append(f"{self.indent} ")
+        else:
+            lines.append(f"{self.f8(ct/cc)} ")
+
+        lines.append(self.func_std_string(func))
+
+        return "".join(lines)
+
+    def get_print_list(self) -> tuple[int, int]:
+        width = self.stats.max_name_len
+
+        if self.stats.fcn_list:
+            stat_list = self.stats.fcn_list[:]
+        else:
+            stat_list = list(self.stats.stats.keys())
+
+        count = len(stat_list)
+
+        if not stat_list:
+            return 0, stat_list
+
+        if count < len(self.stats.stats):
+            width = 0
+            for func in stat_list:
+                if len(self.func_std_string(func)) > width:
+                    width = len(self.func_std_string(func))
+
+        return width + 2, stat_list
+
+    def present(self) -> str:
+        lines = []
+
+        for filename in self.stats.files:
+            lines.append(filename)
+
+        for func in self.stats.top_level:
+            lines.append(f"{self.indent}{func[2]}")
+
+        lines.append(f"{self.stats.total_calls} function calls ")
+
+        if self.stats.total_calls != self.stats.prim_calls:
+            lines.append(f"({self.stats.prim_calls!r} primitive calls) ")
+
+        lines.append(f"in {self.stats.total_tt:.3f} seconds\n\n")
+
+        width, list = self.get_print_list()
+
+        if list:
+            lines.append("Ordered by: cumulative time, function name\n\n")
+            lines.append(f"{self.title}\n")
+            for func in list:
+                lines.append(f"{self.print_line(func)}\n")
+
+        return "".join(lines)
+
+
 def around(before: Optional[Callable] = None, after: Optional[Callable] = None) -> Callable:
     """Add additional behavior before and after execution of decorated function.
 
@@ -31,6 +133,9 @@ def around(before: Optional[Callable] = None, after: Optional[Callable] = None) 
 
     This memory is transferred in the form of additional parameter ("_pipe")
     in the kwargs named argument dictionary.
+
+    Raises:
+        ValueError: If one of the handlers (`before`, `after`) is not specified.
 
     Example::
 
@@ -51,11 +156,31 @@ def around(before: Optional[Callable] = None, after: Optional[Callable] = None) 
         # before!
         # in da func
         # after: some data (from before to after handlers) !
+
+        # !!! Use around with only BEFORE handler !!!
+        @around(before=before_handler)
+        def func2():
+            print("in da func2")
+        # before!
+        # in da func2
+
+        # !!! Use around with only AFTER handler !!!
+        @around(after=after_handler)
+        def func3():
+            print("in da func3")
+        # after!
+        # in da func3
     """
 
     def decorate(func) -> CallableAnyT:
         @wraps(func)
         def wrapper(*args, **kwargs):
+            if not before and not after:
+                raise ValueError(
+                    "One of the handlers (`before`, `after`) is not specified. Read the doc - "
+                    "https://p3t3rbr0.github.io/py3-pure-utils/refs/debug.html#debug.around"
+                )
+
             _buffer = {}
             _args, _kwargs = (deepcopy(args), kwargs.copy())
 
@@ -89,12 +214,12 @@ def caller(at_frame: int = DEFAULT_STACK_FRAME) -> str:
         from pure_utils import caller
 
         def func1(*args, **kwargs):
-            print(f"I'am 'func1'. '{caller()}' called me.")
+            print(f"I'am 'func1', '{caller()}' called me.")
 
         def func2(*args, **kwargs):
             return func1()
 
-        func2()  # I'am 'func1'. 'func2' called me.
+        func2()  # I'am 'func1', 'func2' called me.
     """
     return str(stack()[at_frame].function)
 
@@ -107,17 +232,39 @@ def deltatime(logger: Optional[Logger] = None) -> Callable:
 
     Example::
 
-        ...
+        from pure_utils import deltatime
+
+        @deltatime()
+        def aim_func():
+            for _ in range(1, 1000_000):
+                ...
+
+        result, delta = aim_func()
+        print(f"Execution time of aim_func: {delta} sec.")
+        # Execution time of aim_func: 0.025 sec.
+
+        # !!! Or use decorator with logger (side effect) !!!
+
+        from logging import getLogger, DEBUG, basicConfig
+        basicConfig(level=DEBUG)
+
+        @deltatime(logger=getLogger())
+        def aim_func2():
+            for _ in range(1, 1000_000):
+                ...
+
+        result, _ = aim_func2()
+        # DEBUG:root:[DELTATIME]: 'aim_func2' (0.025 sec.)
     """
 
     def decorate(func) -> CallableAnyT:
         @wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(*args, **kwargs) -> tuple[Any, float]:
             t0 = time()
             retval = func(*args, **kwargs)
             delta = float(f"{time() - t0:.3f}")
             if logger:
-                logger.debug(f"[DELTATIME]: '{func.__name__}' ({delta} sec.)")
+                logger.log(msg=f"[DELTATIME]: '{func.__name__}' ({delta} sec.)", level=logger.level)
             return retval, delta
 
         return wrapper
@@ -134,26 +281,61 @@ def profileit(logger: Optional[Logger] = None, stack_size: int = DEFAULT_STACK_S
 
     Example::
 
+    from pure_utils import profileit
+
+    def func1():
         ...
+
+    def func2():
+        func1()
+
+    def func3():
+        func2()
+
+    @profileit()
+    def func4():
+        func3()
+
+    _, profile_info = func4()
+    print(profile_info)
+    # 5 function calls in 0.000 seconds
+    #    Ordered by: cumulative time, function name
+    #    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+    #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
+    #         1    0.000    0.000    0.000    0.000 scriptname.py:13(func4)
+    #         1    0.000    0.000    0.000    0.000 scriptname.py:10(func3)
+    #         1    0.000    0.000    0.000    0.000 scriptname.py:7(func2)
+    #         1    0.000    0.000    0.000    0.000 scriptname.py:4(func1)
+    # <pstats.Stats object at 0x10cf1a390>
+
+    # !!! Or use decorator with logger (side effect) !!!
+
+    from logging import getLogger, DEBUG, basicConfig
+    basicConfig(level=DEBUG)
+
+    @profileit(logger=getLogger())
+    def func4():
+        func3()
+
+    func4()
     """
     profiler = Profile()
 
     def decorate(func) -> CallableAnyT:
         @wraps(func)
-        def wrapper(*args, **kwargs):
-            result = None
+        def wrapper(*args, **kwargs) -> tuple[Any, Stats]:
+            retval = None
             try:
-                result = profiler.runcall(func, *args, **kwargs)
+                retval = profiler.runcall(func, *args, **kwargs)
             finally:
-                stack_info = (
-                    Stats(profiler)
-                    .strip_dirs()
-                    .sort_stats("cumulative", "name")
-                    .print_stats(stack_size)
-                )
+                profiler_stats = Stats(profiler).strip_dirs().sort_stats("cumulative", "name")
+
+                # s = stats2str(profiler_stats, stack_size)
+                s = ProfileResultPresenter(profiler_stats, stack_size).present()
                 if logger:
-                    logger.debug(f"[PROFILEIT]: {stack_info}")
-            return result, stack_info
+                    # logger.log(msg=f"[PROFILEIT]: {s}", level=logger.level)
+                    print(s)
+            return retval, profiler_stats
 
         return wrapper
 
