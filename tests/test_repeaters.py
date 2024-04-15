edcec7ce99708c65c9e42a0b3bee00647e98aef6
PeterBro <p3t3rbr0@gmail.com>
2024-04-15 07:34:40 +0300

    [repeaters] Add blank tests

---
+++ tests/test_repeaters.py
@@ -0,0 +1,18 @@
+import pytest
+
+from pure_utils.repeaters import ExceptionBasedRepeater, PredicativeBasedRepeater, repeat
+
+
+class TestExceptionBasedRepeater:
+    def test_sample(self):
+        assert True
+
+
+class TestPredicativeBasedRepeater:
+    def test_sample(self):
+        assert True
+
+
+class TestRepeat:
+    def test_sample(self):
+        assert True
